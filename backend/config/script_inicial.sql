6e3bf3caa8d6ee53b8ef4f4cb9a901b9246039c8
gaspar <liboreirogaspar@gmail.com>
2025-06-09 15:07:34 -0300

    fix script_inicial.sql para arreglar el ON DELETE CASCADE

--- backend/config/script_inicial.sql
+++ backend/config/script_inicial.sql
@@ -1,71 +1,154 @@
-/*****************TODA ESTA PRIMER PARTE SE DEBE EJECUTAR COMO ROOT************/
-/*Crear la base de datos*/
-CREATE DATABASE IF NOT EXISTS students_db_3
-CHARACTER SET utf8 
-COLLATE utf8_unicode_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-06-2025 a las 18:06:15
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-/*Crear usuario de la base de datos*/
-CREATE USER 'students_user_3'@'localhost' IDENTIFIED BY '12345';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*Otorgar todos los permisos sobre la base de datos*/
-GRANT ALL PRIVILEGES ON students_db_3.* TO 'students_user_3'@'localhost';
 
-/*Aplicar los cambios en los permisos​*/
-FLUSH PRIVILEGES;
-/******************************************************************************/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-/*************A PARTIR DE ACÁ SE PUEDE HACER COMO ROOT 
-O PARA MAYOR SEGURIDAD CON EL USUARIO students_user_3****************************/
-/*Usar la base de datos​ o ingresar en el Adminer o PHPMyAdmin a la base de datos*/
-USE students_db_3;
+--
+-- Base de datos: `students_db_3`
+--
 
-/*Crear la tabla students*/
-CREATE TABLE IF NOT EXISTS students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    fullname VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    age INT NOT NULL
-) ENGINE=INNODB;
+-- --------------------------------------------------------
 
-/*Insertar algunos datos de prueba*/
-INSERT INTO students (fullname, email, age) VALUES
-('Ana García', 'ana@example.com', 21),
-('Lucas Torres', 'lucas@example.com', 24),
-('Marina Díaz', 'marina@example.com', 22);
+--
+-- Estructura de tabla para la tabla `students`
+--
 
-/*Crear la tabla subjects*/
-CREATE TABLE subjects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE
-) ENGINE=INNODB;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*Insertar materias de prueba*/
-INSERT INTO subjects (name) VALUES 
-('Tecnologías A'), 
-('Tecnologías B'), 
-('Algoritmos y Estructura de Datos I'), 
-('Fundamentos de Informática');
+--
+-- Volcado de datos para la tabla `students`
+--
 
-/*Crear TABLA INTERMEDIA students_subjects
-Constraints, o restricción UNIQUE(student_id, subject_id): garantiza que un estudiante
-no tenga dos veces la misma materia*/
-/*approved: si está aprobada la materia o no (por defecto FALSE).
-ON DELETE CASCADE: si eliminás un estudiante o materia, se borra
-su asignación automáticamente.*/
-CREATE TABLE students_subjects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id INT NOT NULL,
-    subject_id INT NOT NULL,
-    approved BOOLEAN DEFAULT FALSE,
-    UNIQUE (student_id, subject_id),
-    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
-    FOREIGN KEY (subject_id) REFERENCES subjects(id) ON DELETE CASCADE
-) ENGINE=INNODB;
+INSERT INTO `students` (`id`, `fullname`, `email`, `age`) VALUES
+(2, 'Lucas Torres', 'lucas@example.com', 24),
+(3, 'Marina Díaz', 'marina@example.com', 22);
 
-/*Insertar relaciones de prueba students_subjects*/
-INSERT INTO students_subjects (student_id, subject_id, approved) VALUES
-(1, 1, 1),
-(2, 2, 0);
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `students_subjects`
+--
+
+CREATE TABLE `students_subjects` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `approved` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `students_subjects`
+--
+
+INSERT INTO `students_subjects` (`id`, `student_id`, `subject_id`, `approved`) VALUES
+(2, 2, 2, 0),
+(5, 3, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`) VALUES
+(3, 'Algoritmos y Estructura de Datos I'),
+(69, 'Algoritmos y Estructura de Datos II'),
+(4, 'Fundamentos de Informática'),
+(1, 'Tecnologías A'),
+(2, 'Tecnologías B'),
+(10, 'Tecnologías C');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `students_subjects`
+--
+ALTER TABLE `students_subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`,`subject_id`),
+  ADD KEY `fk_subject_id` (`subject_id`);
+
+--
+-- Indices de la tabla `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `students_subjects`
+--
+ALTER TABLE `students_subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `students_subjects`
+--
+ALTER TABLE `students_subjects`
+  ADD CONSTRAINT `fk_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  ADD CONSTRAINT `fk_subject_id` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 /*VOLVER TODO A CERO, BORRAR BASE DE DATOS Y USUARIO (SE DEBERÍA EJECUTAR COMO ROOT)*/
 /*REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'students_user_3'@'localhost';
